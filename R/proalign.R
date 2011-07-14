6fdcdc1ef9aed06d550930cfce3dbeca3253ffab
Ling-Yun Wu <wulingyun@gmail.com>
2011-07-14 23:52:12 +0800

    Add optional arguments

--- R/proalign.R
+++ R/proalign.R
@@ -9,27 +9,21 @@ afp.dist <- function(D1, D2, n.nodes, n.afp, afp.length = 5)
 	.Call("AFP_Distance", D1, D2, n.nodes, n.afp, afp.length)
 }
 
-pro.align <- function(D1, D2)
+pro.align <- function(D1, D2, gap.penalty = 0.1, allow.mid.gap = T, afp.length = 5, afp.cutoff = 1)
 {
-	afp.cutoff <- 1
-	afp.length <- 5
-	gap.penalty <- 0.5
-
-	m.afp <- afp.score(D1, D2, afp.length)
-	m.afp[m.afp >= afp.cutoff] <- 1
-	m.afp <- 1 - m.afp
-
 	n.d1 <- dim(D1)[1]
 	n.d2 <- dim(D2)[1]
-
 	n.nodes <- n.d1
 	n.afp <- n.d2 - afp.length + 1
 	n.states <- n.afp * afp.length + 2
 
 	adj <- matrix(0, nrow=n.nodes, ncol=n.nodes)
 	adj[cbind(1:(n.nodes-1), 2:n.nodes)] <- 1
-
 	crf <- make.crf(adj, n.states)
+
+	m.afp <- afp.score(D1, D2, afp.length)
+	m.afp[m.afp >= afp.cutoff] <- 1
+	m.afp <- 1 - m.afp
 
 	crf$node.pot[,] <- 0
 	crf$node.pot[,1:2] <- gap.penalty
@@ -38,7 +32,8 @@ pro.align <- function(D1, D2)
 
 	ep <- matrix(0, nrow=n.states, ncol=n.states)
 	ep[1,1:(n.afp+2)] <- 1
-	ep[,2] <- 1
+	ep[c(2,(n.afp*(afp.length-1)+3):(n.afp*afp.length+2)),2] <- 1
+	if (allow.mid.gap) ep[2,2:(n.afp+2)] <- 1
 	ep[cbind(3:(n.afp*(afp.length-1)+2), (n.afp+3):(n.afp*afp.length+2))] <- 1
 
 	m.dist <- afp.dist(D1, D2, n.nodes, n.afp, afp.length)
