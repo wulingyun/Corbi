226a0a0a44744b5ab00b10355884d253a5917162
Ling-Yun Wu <wulingyun@gmail.com>
2011-09-22 11:12:17 +0800

    Improve the speed of shortest paths algorithm

--- src/dis.cpp
+++ src/dis.cpp
@@ -1,16 +1,5 @@
-#include <R.h>
-#include <Rdefines.h>
-#include <Rmath.h>
 #include "Corbi.h"
-
 #include "FibHeap.h"
-#include <stdlib.h>
-//#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <string> 
-#include <cstdio>
-using   namespace   std;
 
 class HeapNode: public FibHeapNode
 {
@@ -21,8 +10,8 @@ public:
 	HeapNode() : FibHeapNode() { m_label = 0; };
 
 	virtual void operator =(FibHeapNode& RHS);
-	virtual int  operator ==(FibHeapNode& RHS);
-	virtual int  operator <(FibHeapNode& RHS);
+	virtual int operator ==(FibHeapNode& RHS);
+	virtual int operator <(FibHeapNode& RHS);
 
 	virtual void operator =(double key);
 
@@ -46,82 +35,18 @@ void HeapNode::operator =(FibHeapNode& RHS)
 	m_label = ((HeapNode&) RHS).m_label;
 }
 
-int  HeapNode::operator ==(FibHeapNode& RHS)
+int HeapNode::operator ==(FibHeapNode& RHS)
 {
 	if (FHN_compare(RHS)) return 0;
 	return m_label == ((HeapNode&) RHS).m_label ? 1 : 0;
 }
 
-int  HeapNode::operator <(FibHeapNode& RHS)
+int HeapNode::operator <(FibHeapNode& RHS)
 {
 	int X;
 
 	if ((X=FHN_compare(RHS)) != 0) return X < 0 ? 1 : 0;
 	return m_label < ((HeapNode&) RHS).m_label ? 1 : 0;
-}
-
-
-void onenoded(double *W,int d1,int s,double *D){
-	// ** the pointer of the weight matrix
-	// d1 the dimension of the matrix
-	// s the source node 
-	// * the one row pointer of the distance matrix 
-
-    HeapNode *label, *min_label, temp_label;
-	FibHeap heap;
-	double  current_value,d;	
-	int *backtrack;
-	int i, j;
-	bool *label_free;
-
-	label_free = new bool [d1];
-	backtrack = new int [d1];
-	label = new HeapNode [d1];
-
-
-	// Dijkstra's algorithm
-	// initial the nodes
-		for (i=0; i<d1; i++) {
-			if(i!=s){
-		    label[i].setKeyValue(HUGE_VAL);
-			label[i].setIndexValue(i);
-			heap.insert(&label[i]);
-			label_free[i] = true;
-			backtrack[i] = -1;}
-			else{
-		    label[i].setKeyValue(0);
-			label[i].setIndexValue(i);
-			heap.insert(&label[i]);
-			label_free[i] = true;
-			backtrack[i] = -1;			
-			}
-		}
-
-    // do while
-		while(true){	
-			min_label = (HeapNode *)heap.extractMin();				
-			if (min_label == NULL || min_label->getKeyValue() >= HUGE_VAL) break;
-			i = min_label->getIndexValue();
-			current_value = min_label->getKeyValue();
-			label_free[i] = false;
-			D[i+s*d1]=min_label->getKeyValue();
-			
-            for (j=0;j<d1;j++){
-				if(W[i*d1+j]!=0 && label_free[j]){ // i ,j is neighbor
-					d = current_value + W[i*d1+j];
-					if (d < label[j].getKeyValue()) {
-							temp_label = label[j];
-							temp_label.setKeyValue(d);
-							heap.decreaseKey(&label[j], temp_label);
-							backtrack[j] = i;
-						}
-				}
-			}
-		}
-
-	delete[] label;
-	delete[] backtrack;
-	delete[] label_free;
 }
 
 SEXP dis(SEXP _W, SEXP _d1)
@@ -134,15 +59,76 @@ SEXP dis(SEXP _W, SEXP _d1)
 	PROTECT(_D = NEW_NUMERIC(d1*d1));
 	setDim2(_D, d1, d1);
 	double *D = NUMERIC_POINTER(_D);
+	setValues(_D, D, 0);
 
-	int i,j;
+	int *out_links = (int *) R_alloc(d1*d1, sizeof(int));
+	int *out_degree = (int *) R_alloc(d1, sizeof(int));
 
-	
-	for (i=0;i<d1;i++){for(j=0;j<d1;j++){D[i+j*d1]=0;}}
-	
-	for (i=0;i<d1;i++){
-	onenoded(W,d1,i,D);
+	for (int i = 0; i < d1; i++)
+	{
+		out_degree[i] = 0;
+		for (int j = 0; j < d1; j++)
+		{
+			if (W[i + j * d1] != 0)
+			{
+				out_links[i*d1 + out_degree[i]] = j;
+				out_degree[i]++;
+			}
+		}
 	}
+
+	bool *label_free;
+	HeapNode *label;
+
+	label_free = (bool *) R_alloc(d1, sizeof(bool));
+	label = new HeapNode [d1];
+
+	HeapNode *min_label, temp_label;
+	int min_index;
+	FibHeap heap;
+	double min_value, d;
+
+	for (int s = 0; s < d1; s++)
+	{
+		// Dijkstra's algorithm
+
+		// initial the nodes
+		for (int i = 0; i < d1; i++)
+		{
+			label[i].setKeyValue(i != s ? HUGE_VAL : 0);
+			label[i].setIndexValue(i);
+			heap.insert(&label[i]);
+			label_free[i] = true;
+		}
+
+		// do while
+		while (true)
+		{	
+			min_label = (HeapNode *) heap.extractMin();				
+			if (min_label == NULL || min_label->getKeyValue() >= HUGE_VAL)
+				break;
+			min_index = min_label->getIndexValue();
+			D[s + min_index * d1] = min_value = min_label->getKeyValue();
+			label_free[min_index] = false;
+				
+			for (int j = 0; j < out_degree[min_index]; j++)
+			{
+				int k = out_links[min_index * d1 + j];
+				if (label_free[k]) // i, k is neighboring
+				{
+					d = min_value + W[min_index + k * d1];
+					if (d < label[k].getKeyValue())
+					{
+						temp_label = label[k];
+						temp_label.setKeyValue(d);
+						heap.decreaseKey(&label[k], temp_label);
+					}
+				}
+			}
+		}
+	}
+
+	delete[] label;
 
 	UNPROTECT(2);
 	return (_D);
