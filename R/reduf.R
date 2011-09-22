8cfea0aeb2903491bbf1a819932b303b20133852
qhuang <qhuang@Nature.ZHANGroup.local>
2011-09-22 15:11:55 +0800

    change the distance to itself

--- R/reduf.R
+++ R/reduf.R
@@ -8,11 +8,11 @@ b<-s>0;
 g=G[b,b];
 
 if(length(g)>0){
-g[g==0]=Inf; ## this may be change as the distance matrix method
+g[g==-1]=Inf; ## this may be change as the distance matrix method
 
 name<-c("");
 name=N[b];
-for (j in 1:dim(g)[1]) {if(g[j,j]==Inf){g[j,j]=2;}}
+g[cbind(1:dim(g)[1],1:dim(g)[1])]=1;
 
 E1<-E[,b];}
 else{print("The simplification network is empty, you should give a smaller cut values!")}
