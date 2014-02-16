ca3eda0deb903c002b4cf0b98e8c31d1fc1c7f52
Ling-Yun Wu <wulingyun@gmail.com>
2014-02-16 22:29:32 +0800

    Improve the speed of rmultihyper

--- src/multihyper.cpp
+++ src/multihyper.cpp
@@ -8,28 +8,30 @@ SEXP RMultiHyper(SEXP _N, SEXP _M, SEXP _K)
 
   PROTECT(_M = AS_INTEGER(_M));
   int *M = INTEGER_POINTER(_M);
-  int len = length(_M);
+  int nM = length(_M);
 
   SEXP _X;
-  PROTECT(_X = NEW_INTEGER(len * N));
-  setDim2(_X, len, N);
+  PROTECT(_X = NEW_INTEGER(nM * N));
+  setDim2(_X, nM, N);
   int *X = INTEGER_POINTER(_X);
+
+  int sumM = 0;
+  for (int i = 0; i < nM; i++)
+      sumM += M[i];
 
   GetRNGstate();
   for (int n = 0; n < N; n++)
   {
-    int nM = 0;
-    for (int i = 0; i < len; i++)
-      nM += M[i];
-    int nK = K;
-    for (int i = 1; i < len; i++)
+    int restM = sumM;
+    int restK = K;
+    for (int i = 1; i < nM; i++)
     {
-      nM -= M[i];
-      X[i] = rhyper(M[i], nM, nK);
-      nK -= X[i];
+      restM -= M[i];
+      X[i] = rhyper(M[i], restM, restK);
+      restK -= X[i];
     }
-    X[0] = nK;
-    X += len;
+    X[0] = restK;
+    X += nM;
   }
   PutRNGstate();
   
