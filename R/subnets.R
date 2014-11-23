15aa8dbd2d18552931ec9f0541f23bd49e5984c7
Ling-Yun Wu <wulingyun@gmail.com>
2014-11-24 00:00:22 +0800

    Add function best_subnets

--- R/subnets.R
+++ R/subnets.R
@@ -1,3 +1,32 @@
+#' Find the optimal sub-networks
+#' 
+#' @export
+best_subnets <- function(func, net.matrix, max.size = 10, exhaust.size = 5, max.top = 10000)
+{
+  subnets <- get_subnets(net.matrix, exhaust.size)
+  results <- lapply(subnets, function (x) apply(x, 1, func))
+  orders <- lapply(results, function(x) order(x, decreasing = TRUE))
+  subnets <- lapply(1:exhaust.size, function(x) matrix(subnets[[x]][orders[[x]], ], ncol = x))
+  results <- lapply(1:exhaust.size, function(x) results[[x]][orders[[x]]])
+  i <- exhaust.size + 1
+  while (i <= max.size)
+  {
+    sub1 <- subnets[[i-1]]
+    top <- min(max.top, dim(sub1)[1])
+    sub1 <- matrix(sub1[1:top, ], ncol = i-1)
+    if (top > 1) sub2 <- sub1
+    else sub2 <- subnets[[2]]
+    subnets[[i]] <- extend_subnets(sub1, sub2, i)
+    results[[i]] <- apply(subnets[[i]], 1, func)
+    orders <- order(results[[i]], decreasing = TRUE)
+    subnets[[i]] <- matrix(subnets[[i]][orders, ], ncol = i)
+    results[[i]] <- results[[i]][orders]    
+    i <- i + 1
+  }
+  list(subnets=subnets, results=results)
+}
+
+
 #' Get all sub-networks
 #' 
 #' @export
