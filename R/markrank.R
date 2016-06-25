f7c4bd3a87c1efc2fee9f1efde3c1030bdf518c5
Ling-Yun Wu <wulingyun@gmail.com>
2016-06-25 14:19:17 +0800

    Add documentation of MarkRank

--- R/markrank.R
+++ R/markrank.R
@@ -1,37 +1,61 @@
+#' MarkRank
+#' 
+#' Prioritization of network biomarkers for complex diseases
+#' 
+#' MarkRank is a network biomarker identification method to prioritize disease genes 
+#' by integrating multi-source information including the biological network, eg. 
+#' protein-protein interaction (PPI) network, the prior information about related diseases,
+#' and the discriminative power of cooperative gene combinations.
+#' 
+#' @param dataset The microarray expression matrix of related disease. Each row represents
+#' a sample and each column represents a gene.
+#' @param label The 0-1 binary phenotype vector of dataset samples. The size of label must
+#' accord with the sample number in dataset.
+#' @param adj_matrix The 0-1 binary adjacent matrix of a connected biological network. 
+#' Here the node set should be the same order as the gene set in expression matrix. 
+#' @param alpha The convex combination coefficient of network effect and prior information vector E.
+#' @param lambda The convex combination coefficient of two network effects.
+#' @param eps The stop criteria for the iterative solution method.
+#' @param E_value A vector containing the prior information about the importance of nodes. Default is 
+#' absolute Pearson correlation coefficient (PCC).
+#' @param trace Locaical variable indicated whether tracing information on the progress of the iterative
+#' solution method is produced.
+#' 
+#' @return This function will return a list with components:
+#'   \item{score}{The vector of final MarkRank scores for each gene.}
+#'   \item{steps}{The final steps used by iterative method.}
+#'   \item{NET2}{The weighted adjacent matrix of discriminative potential network.}
+#'   \item{initial_pars}{The initial parameter values used in computation.}
+#' 
+#' @references Duanchen Sun, Xianwen Ren, Eszter Ari, Tamas Korcsmaros, Peter Csermely,
+#' Ling-Yun Wu. Prioritization of network biomarkers for complex diseases via MarkRank.
+#' Manuscript, 2016.
+#' 
 #' @import Matrix
 #' 
 #' @export
-markrank <- function(dataset, label, PPI_adj, alpha, lambda, eps=1e-10, E_value=NULL, trace=TRUE)
+markrank <- function(dataset, label, adj_matrix, alpha, lambda, eps=1e-10, E_value=NULL, trace=TRUE)
 {
-  # INPUT DESCRIPTION: 
-  # dataset : The microarray expression matrix of related disease. Each row represents a sample and each column represents a gene.
-  # label   : The 0-1 binary phenotype vector of dataset samples. The size of label must accordance with the sample number in dataset.
-  # PPI_adj : The 0-1 binary adjacent matrix of a connected biological network. Here the node set should be the same order as the gene set in expression matrix. 
-  
-  # The coefficient matrix A = lambda*A1 + (1-lambda)*A2. 
-  # The coefficient matrix A1 and A2 are derived from NET1 and NET2 (the adjacent matrix of original PPI network and the discriminative potential network) respectively.
-  # The matrix equation is : (I-alpha*A)*R = (1-alpha)*E, where the I is n*n unit matrix.
-  # Solving the final matrix equation by iterative method or matrix inversion method. First one is preferred.
-  
-  # E_value : Contain the prior information about the importance of nodes. Default is absolute Pearson correlation coefficient (PCC).
-  # alpha   : The convex combination coefficient of network effect and prior information vector E.
-  # lambda  : The convex combination coefficient of two network effects.
-  # eps     : The cut-off value of the iteration solving method.
-
   m <- nrow(dataset)												# Sample number.
   n <- ncol(dataset)												# Gene number.
-  if (length(label) != m){
-    stop("The size of label must accordance with the sample number in dataset.")
+  if (length(label) != m) {
+    stop("The size of label must accord with the sample number in dataset.")
   }
-  if (!setequal(rownames(PPI_adj), colnames(dataset))){
-    stop("The mapping for genes is not performed")
+  if (!setequal(rownames(adj_matrix), colnames(dataset))) {
+    stop("The mapping for genes is not performed.")
   }
-  NET1 <- PPI_adj
+  NET1 <- adj_matrix
   degs <- rowSums(NET1)
-  isos <- which(degs == 0)
-  if (length(isos) > 0){
+  if (sum(degs == 0) > 0) {
     stop("The input biological network should be a connected network.")
   }	
+
+# The coefficient matrix A = lambda*A1 + (1-lambda)*A2.
+# The coefficient matrix A1 and A2 are derived from NET1 and NET2 (the adjacent matrix of 
+# original biological network and the discriminative potential network) respectively.
+# The matrix equation is : (I-alpha*A)*R = (1-alpha)*E, where the I is n*n unit matrix.
+# Solving the final matrix equation by iterative method or matrix inversion method. 
+# First one is preferred.
   
   D1 <- diag(1/degs)
   A1 <- t(NET1)%*%D1
@@ -76,7 +100,7 @@ markrank <- function(dataset, label, PPI_adj, alpha, lambda, eps=1e-10, E_value=
   label <- as.matrix(as.numeric(label))
   MI1 <- mminjk(dataset, label, level=0L, na.rm=FALSE)
   MI2 <- Matrix(0, l, l, dimnames=list(colnames(dataset),colnames(dataset)), sparse=TRUE)
-  for (i in 1:(l-1)){
+  for (i in 1:(l-1)) {
     if (trace == TRUE && i%%10 == 0) print(i)
     dataset_tmp <- (dataset[,(i+1):l, drop=FALSE] + dataset[,i])/sqrt(2)
     MI2[(i+1):l, i] <- mminjk(dataset_tmp, label, level=0L, na.rm=FALSE)
