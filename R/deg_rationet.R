b09f753dd629f935af542c83a540b4a7bb97d25d
Ling-Yun Wu <wulingyun@gmail.com>
2018-05-12 00:55:29 +0800

    Refine netDEG functions and arguments

--- R/deg_rationet.R
+++ R/deg_rationet.R
@@ -1,17 +1,16 @@
 
 #' @export
-netDEG <- function(ratio.dist, expr)
+netDEG <- function(ref.ratio.dist, expr)
 {
-  net <- getDiffRatioNet(ratio.dist, expr)
-  PvalueNetDEG(net, ratio.dist)
+  PvalueNetDEG(ref.ratio.dist, getDiffRatioNet(ref.ratio.dist, expr))
 }
 
 
 #' @export
-PvalueNetDEG <- function(adj.matrix, ratio.dist)
+PvalueNetDEG <- function(ref.ratio.dist, net)
 {
-  score <- getAdjustedDiff(adj.matrix)
-  pvalue <- pnbinom(abs(score), size = ratio.dist$NB['size'], mu = ratio.dist$NB['mu'], lower.tail = FALSE)
+  score <- .netDEG.getAdjustedDiff(net)
+  pvalue <- pnbinom(abs(score), size = ref.ratio.dist$NB['size'], mu = ref.ratio.dist$NB['mu'], lower.tail = FALSE)
   p = pvalue * 0.5
   up = ifelse(score > 0, p, 1-p)
   down = ifelse(score < 0, p, 1-p)
@@ -23,24 +22,23 @@ PvalueNetDEG <- function(adj.matrix, ratio.dist)
 #'
 #'
 #' @export
-getRatioDistribution <- function(expr, p.edge = 0.1)
+getRatioDistribution <- function(ref.expr.matrix, p.edge = 0.1)
 {
-  dist <- .Call(ND_RatioDistribution, expr, p.edge)
-  diff <- as.vector(sapply(1:dim(expr)[2], function(i) getAdjustedDiff(getDiffRatioNet(dist, expr[,i]))))
+  dist <- .Call(ND_RatioDistribution, ref.expr.matrix, p.edge)
+  diff <- as.vector(sapply(1:dim(ref.expr.matrix)[2], function(i) .netDEG.getAdjustedDiff(getDiffRatioNet(dist, ref.expr.matrix[,i]))))
   dist$NB <- MASS::fitdistr(abs(diff), "negative binomial", lower = c(1e-10, 1e-10))$estimate
   dist
 }
 
 
 #' @export
-getDiffRatioNet <- function(ratio.dist, expr)
+getDiffRatioNet <- function(ref.ratio.dist, expr)
 {
-  .Call(ND_DiffRatioNet, ratio.dist$LB, ratio.dist$UB, expr)
+  .Call(ND_DiffRatioNet, ref.ratio.dist$LB, ref.ratio.dist$UB, expr)
 }
 
 
-#' @export
-getAdjustedDiff <- function(net, p = 0.5)
+.netDEG.getAdjustedDiff <- function(net, p = 0.5)
 {
   n.gene <- dim(net)[1]
   d.out <- rowSums(net)
