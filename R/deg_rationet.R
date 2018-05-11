85efe36d22c126ad3ca7342c304efe3255ce0954
Ling-Yun Wu <wulingyun@gmail.com>
2018-05-12 00:29:07 +0800

    Remove deg_rankcomp.R and clean unused function

--- R/deg_rationet.R
+++ R/deg_rationet.R
@@ -49,31 +49,3 @@ getAdjustedDiff <- function(net, p = 0.5)
   d.diff <- d.out - d.in
   d.diff - ceiling(median(d.diff[d.sum <= quantile(d.sum, p)]))
 }
-
-#' @import Matrix
-#
-#' @export
-random_net <- function(size, p.edge, sparse = TRUE)
-{
-  max.edge <- choose(size, 2)
-  edge.id <- which(runif(max.edge) <= p.edge)
-  edge.dir <- runif(length(edge.id)) <= 0.5
-  edge.1 <- edge.id[edge.dir]
-  edge.2 <- edge.id[!edge.dir]
-
-  rows <- rep.int(2:size, times=1:(size-1))
-  cols <- sequence(1:(size-1))
-  i <- c(rows[edge.1], cols[edge.2])
-  j <- c(cols[edge.1], rows[edge.2])
-
-  if (sparse)
-  {
-    adj.matrix <- sparseMatrix(i, j, x=TRUE, dims=c(size, size))
-  }
-  else
-  {
-    adj.matrix <- matrix(FALSE, size, size)
-    adj.matrix[cbind(i, j)] <- TRUE
-  }
-  adj.matrix
-}
